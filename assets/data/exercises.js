270b2cf2b042d4cceceae9eeaecdf01ff0df0b24
jaejunkim <aamoof@gmail.com>
2026-08-17 14:57:14 +0900

    Separate fitness records into data files

---
+++ assets/data/exercises.js
@@ -0,0 +1,45 @@
+window.exerciseCatalog = Object.freeze([
+  {
+    id: "cable-one-arm-lateral-raise",
+    name: "로우 케이블 원암 레터럴 레이즈",
+    primaryMuscles: ["측면 어깨"],
+    secondaryMuscles: []
+  },
+  {
+    id: "seated-row",
+    name: "시티드 로우",
+    primaryMuscles: ["등"],
+    secondaryMuscles: ["광배근", "후면 어깨", "이두"]
+  },
+  {
+    id: "cable-overhead-triceps-extension",
+    name: "케이블 오버헤드 트라이셉스 익스텐션",
+    primaryMuscles: ["삼두"],
+    secondaryMuscles: []
+  },
+  {
+    id: "assisted-pull-up-machine",
+    name: "어시스트 풀업 머신",
+    primaryMuscles: ["광배근"],
+    secondaryMuscles: ["등", "이두"],
+    loadNote: "기구 숫자가 보조 중량인지 확인되기 전까지 표시 중량으로 기록"
+  },
+  {
+    id: "reverse-pec-deck-fly",
+    name: "리버스 펙덱 플라이",
+    primaryMuscles: ["후면 어깨"],
+    secondaryMuscles: ["등"]
+  },
+  {
+    id: "dumbbell-shoulder-press",
+    name: "덤벨 숄더프레스",
+    primaryMuscles: ["전면 어깨"],
+    secondaryMuscles: ["측면 어깨", "삼두"]
+  },
+  {
+    id: "machine-chest-press",
+    name: "머신 체스트프레스",
+    primaryMuscles: ["가슴"],
+    secondaryMuscles: ["전면 어깨", "삼두"]
+  }
+]);
---
+++ assets/data/profile.js
@@ -0,0 +1,6 @@
+window.fitnessProfile = Object.freeze({
+  birthYear: 1996,
+  heightCm: 176,
+  sex: "male",
+  defaultReps: 10
+});
